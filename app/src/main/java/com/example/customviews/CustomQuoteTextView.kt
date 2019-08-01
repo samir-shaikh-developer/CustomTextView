5fbd2093c5768e3e7d3dd5c78624195b3848b186
Samir Shaikh <samir@tickledmedia.com>
2019-08-01 17:12:13 +0530

    remove include custom text view

--- app/src/main/java/com/example/customviews/CustomQuoteTextView.kt
+++ app/src/main/java/com/example/customviews/CustomQuoteTextView.kt
@@ -2,24 +2,22 @@ package com.example.customviews
 
 import android.content.Context
 import android.graphics.Canvas
-import android.graphics.Color
 import android.graphics.Paint
 import android.util.AttributeSet
-import android.util.Log
 import android.widget.TextView
 
 class CustomQuoteTextView(context: Context, attrs: AttributeSet?) : TextView(context, attrs) {
 
-        private val leftBarColor: Int
-        private val leftBarWidth: Float
-        private val spaceBetween: Float
-        private val typedArray = context.theme.obtainStyledAttributes(attrs, R.styleable.QuoteCustomView, 0, 0)
+    private val leftBarColor: Int
+    private val leftBarWidth: Float
+    private val spaceBetween: Float
+    private val typedArray = context.theme.obtainStyledAttributes(attrs, R.styleable.QuoteCustomView, 0, 0)
 
     init {
         try {
-            leftBarColor    = typedArray.getColor(R.styleable.QuoteCustomView_leftBarColor, resources.getColor(R.color.colorPrimary))
-            leftBarWidth    = typedArray.getDimension(R.styleable.QuoteCustomView_leftBarWidth, 10.0f)
-            spaceBetween    = typedArray.getDimension(R.styleable.QuoteCustomView_spaceBetween, 15.0f)
+            leftBarColor = typedArray.getColor(R.styleable.QuoteCustomView_leftBarColor, resources.getColor(R.color.colorPrimary))
+            leftBarWidth = typedArray.getDimension(R.styleable.QuoteCustomView_leftBarWidth, 10.0f)
+            spaceBetween = typedArray.getDimension(R.styleable.QuoteCustomView_spaceBetween, 15.0f)
         } finally {
             typedArray.recycle()
         }
@@ -29,7 +27,7 @@ class CustomQuoteTextView(context: Context, attrs: AttributeSet?) : TextView(con
         val p = Paint()
         p.color = leftBarColor
         this.setPadding(spaceBetween.toInt(), this.paddingTop, this.paddingRight, this.paddingBottom)
-        canvas?.drawRect(0.0f, 0.0f, leftBarWidth, this.height.toFloat(), p)
+        canvas?.drawRect(0.0f, 0.0f, leftBarWidth, this.height.toFloat() / this.lineCount, p)
         super.onDraw(canvas)
     }
 }
