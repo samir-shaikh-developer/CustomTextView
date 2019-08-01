8eda930523c75c4c6d3e6a1609588bc77f3e607c
Samir Shaikh <samir@tickledmedia.com>
2019-08-01 17:02:53 +0530

    Quote View implemented with text view

---
+++ app/src/main/java/com/example/customviews/CustomQuoteTextView.kt
@@ -0,0 +1,35 @@
+package com.example.customviews
+
+import android.content.Context
+import android.graphics.Canvas
+import android.graphics.Color
+import android.graphics.Paint
+import android.util.AttributeSet
+import android.util.Log
+import android.widget.TextView
+
+class CustomQuoteTextView(context: Context, attrs: AttributeSet?) : TextView(context, attrs) {
+
+        private val leftBarColor: Int
+        private val leftBarWidth: Float
+        private val spaceBetween: Float
+        private val typedArray = context.theme.obtainStyledAttributes(attrs, R.styleable.QuoteCustomView, 0, 0)
+
+    init {
+        try {
+            leftBarColor    = typedArray.getColor(R.styleable.QuoteCustomView_leftBarColor, resources.getColor(R.color.colorPrimary))
+            leftBarWidth    = typedArray.getDimension(R.styleable.QuoteCustomView_leftBarWidth, 10.0f)
+            spaceBetween    = typedArray.getDimension(R.styleable.QuoteCustomView_spaceBetween, 15.0f)
+        } finally {
+            typedArray.recycle()
+        }
+    }
+
+    override fun onDraw(canvas: Canvas?) {
+        val p = Paint()
+        p.color = leftBarColor
+        this.setPadding(spaceBetween.toInt(), this.paddingTop, this.paddingRight, this.paddingBottom)
+        canvas?.drawRect(0.0f, 0.0f, leftBarWidth, this.height.toFloat(), p)
+        super.onDraw(canvas)
+    }
+}
