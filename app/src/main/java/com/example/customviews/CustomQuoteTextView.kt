942f75e25715553500abeacc1d8c852e743b43d8
Samir Shaikh <samir@tickledmedia.com>
2019-08-01 18:17:48 +0530

    bullet point implemeted

--- app/src/main/java/com/example/customviews/CustomQuoteTextView.kt
+++ app/src/main/java/com/example/customviews/CustomQuoteTextView.kt
@@ -4,6 +4,7 @@ import android.content.Context
 import android.graphics.Canvas
 import android.graphics.Paint
 import android.util.AttributeSet
+import android.util.Log
 import android.widget.TextView
 
 class CustomQuoteTextView(context: Context, attrs: AttributeSet?) : TextView(context, attrs) {
@@ -11,6 +12,9 @@ class CustomQuoteTextView(context: Context, attrs: AttributeSet?) : TextView(con
     private val leftBarColor: Int
     private val leftBarWidth: Float
     private val spaceBetween: Float
+    private val circleRadius: Float
+    private val shapeType: String?
+
     private val typedArray = context.theme.obtainStyledAttributes(attrs, R.styleable.QuoteCustomView, 0, 0)
     private val p = Paint()
 
@@ -19,7 +23,11 @@ class CustomQuoteTextView(context: Context, attrs: AttributeSet?) : TextView(con
         try {
             leftBarColor = typedArray.getColor(R.styleable.QuoteCustomView_leftBarColor, resources.getColor(R.color.colorPrimary))
             leftBarWidth = typedArray.getDimension(R.styleable.QuoteCustomView_leftBarWidth, 10.0f)
+            circleRadius = typedArray.getDimension(R.styleable.QuoteCustomView_circleRadius, 5.0f)
             spaceBetween = typedArray.getDimension(R.styleable.QuoteCustomView_spaceBetween, 15.0f)
+            shapeType = typedArray.getString(R.styleable.QuoteCustomView_shapeType)
+
+            Log.d("TAGGG", "Circle Radius $circleRadius")
         } finally {
             typedArray.recycle()
         }
@@ -27,8 +35,22 @@ class CustomQuoteTextView(context: Context, attrs: AttributeSet?) : TextView(con
 
     override fun onDraw(canvas: Canvas?) {
         p.color = leftBarColor
-        this.setPadding(spaceBetween.toInt(), this.paddingTop, this.paddingRight, this.paddingBottom)
-        canvas?.drawRect(0.0f, 0.0f, leftBarWidth, this.height.toFloat() / this.lineCount, p)
+        if (shapeType.equals("rectangle")) {
+            if(leftBarWidth >= spaceBetween) {
+                this.setPadding(leftBarWidth.toInt() + spaceBetween.toInt(), this.paddingTop, this.paddingRight, this.paddingBottom)
+            } else {
+                this.setPadding(spaceBetween.toInt(), this.paddingTop, this.paddingRight, this.paddingBottom)
+            }
+            canvas?.drawRect(0.0f, 0.0f, leftBarWidth, this.height.toFloat() / this.lineCount, p)
+        } else if (shapeType.equals("circle")) {
+            if((circleRadius * 2) >= spaceBetween ) {
+                this.setPadding((circleRadius * 2).toInt() + spaceBetween.toInt(), this.paddingTop, this.paddingRight, this.paddingBottom)
+            } else {
+                this.setPadding(spaceBetween.toInt() , this.paddingTop, this.paddingRight, this.paddingBottom)
+            }
+            canvas?.drawCircle( circleRadius, ((this.height.toFloat() / this.lineCount) / 2),circleRadius, p)
+        }
+
         super.onDraw(canvas)
     }
 }
